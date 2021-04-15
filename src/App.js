617a40aceba317647051e5538e014f7071505a80
dockerlead <dockerlead@gmail.com>
2021-04-14 20:31:27 -0500

    Use map to render iterable state entity

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: 'Hello Crazy'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asdf'
+        },
+        {
+          name: 'Zombie',
+          id: 'ggdd'
+        }
+      ]
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Hello Amtui' })}>Change Text</button>
-        </header>
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+        }
       </div>
     );
   }
