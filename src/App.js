d8420663d4a7fe5b8a264b0dfda8f37d65b222b8
dockerlead <dockerlead@gmail.com>
2021-04-14 23:01:07 -0500

    Add search field

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -21,6 +22,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          type='search'
+          placeholder='Search monsters.'
+          onChange={e => {
+            this.setState({ searchField: e.target.value }, () => console.log(this.state));
+          }}
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
